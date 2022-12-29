dac86197581558751d1177a9db5c2fd1a3edf686
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-12-30 01:16:10 +0530

    section-13 base skeleton ready

---
+++ section-13-share-place-app-project/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/app.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Select & Share place',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
